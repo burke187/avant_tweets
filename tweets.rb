9e9ea7c19734e671b44e43c943179797f270ef6c
William Burke <burke187@gmail.com>
2015-03-31 10:58:34 -0500

    collecting tweets

---
+++ tweets.rb
@@ -0,0 +1,33 @@
+require 'tweetstream'
+
+module Tweets
+	class Stream
+
+		def initialize
+    		TweetStream.configure do |config|
+    			config.consumer_key       = 'd8mwHtVXWjXRWsEp4VuoNuL6U'
+    			config.consumer_secret    = 'S6zB0g4sSTKsVnxdoWWBMFkjkHGQhWfY0fWNVaXaRuLtTkAcpf'
+    			config.oauth_token        = '1280452981-MfMI0Rhtfef3O556EOT4Id8kpjsH3SQeWLqbhSA'
+    			config.oauth_token_secret = 'no7q9T5GJxlttn5K62adeaYzsPA9IcWsNg3wP1dLil099'
+    			config.auth_method        = :oauth
+			end
+		end
+
+		def collect_tweets
+			tweets = []
+			time = 5
+			EM.run do
+				client = TweetStream::Client.new
+				EM::PeriodicTimer.new(10) do
+					client.sample do |tweet|
+						tweets << tweet.text
+						client.stop
+					end
+				end
+			end
+			puts tweets.inspect
+		end
+
+	end
+end
+p Tweets::Stream.new.collect_tweets
